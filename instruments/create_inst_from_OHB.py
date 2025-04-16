1a1f953797455942c8b6d5f1e2a014808b444918
Adrien Martin <admartin@noc.ac.uk>
2025-04-16 15:50:07 +0200

    error on filename str

--- instruments/create_inst_from_OHB.py
+++ instruments/create_inst_from_OHB.py
@@ -32,7 +32,7 @@ def create_OHB_simple_inst(fpath: str, value_along=0, size_across=11, write_nc=F
     inst_file_str = f"inst_{simple_inst.across.size:03d}_{inst_ds.attrs['filename']}.nc"
     if 'filename' in simple_inst.attrs:
         simple_inst.attrs['history'] = simple_inst.attrs['filename']
-        simple_inst.attrs['filename'] = inst_file_str[-3]
+        simple_inst.attrs['filename'] = inst_file_str[:-3]
     if write_nc:
         inst_path = os.path.join(main_path, 'inst')
         pathlib.Path(inst_path).mkdir(parents=True, exist_ok=True)
